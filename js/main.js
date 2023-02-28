55ce5f0385c0b8fbf81187e180bc02775b25600e
mgballou <matthewbuiltthat@gmail.com>
2023-02-28 15:55:14 -0500

    Colors posting to board

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 const colors = {
     null: 'white',
     1: 'yellow',
-    2: 'pink',
+    '-1': 'pink',
 }
 
-const winConditions =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
+const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
 
 
 /*----- state variables -----*/
@@ -39,39 +39,47 @@ function init() {
 }
 
 function render() {
-    
+
     renderBoard()
     renderMessage()
 
-   
+
 }
 
-function renderMessage()) {
-   if (winner) {
-    
-   } 
-   if (currentPlayer === 1)
+function renderMessage() {
+    messageEl.textContent = `${colors[currentPlayer].toUpperCase()}'s turn to play.`
 
 }
 
 function renderBoard() {
     /// loop over 9 elements of the board tiles, and for each one, check the board array at that index to determine if a mark should be placed, then place that color if so
+    gameTileEls.forEach(function (tile) {
 
+        tile.style.backgroundColor = colors[boardArray[tile.id]]})
 }
 
 
 function handleClick(evt) {
-    if (evt.target.tagName !== "DIV") {
-        return
-    }
-    if (evt.target.innerText) {
-        return
-    } else {
-        renderMarks(evt)
+        if (evt.target.tagName !== "DIV") {
+            return
+        }
+        console.log([boardArray[8]])
+        if (boardArray[evt.target.id]) {
+            return
+        }
+        boardArray[evt.target.id] = currentPlayer
+        console.log(boardArray)
+        advanceTurn()
+        render()
 
     }
+
+function advanceTurn() {
+        if (currentPlayer === 1) {
+            currentPlayer = -1
+        } else if (currentPlayer === -1) {
+            currentPlayer = 1
+        }
 }
-
-
 
 init()
