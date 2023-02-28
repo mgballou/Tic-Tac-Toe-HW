5abaeefec782efdcb0eb69b6cb96e760bf67fc88
mgballou <matthewbuiltthat@gmail.com>
2023-02-28 15:54:46 -0500

    Colors posting to board

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 /*----- constants -----*/
+const colors = {
+    null: 'white',
+    1: 'yellow',
+    2: 'pink',
+}
+
+const winConditions =[[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
 
 
 /*----- state variables -----*/
 let currentPlayer
 let winner
+let boardArray
+
 
 
 
@@ -11,6 +20,7 @@ let winner
 const gameBoardEl = document.querySelector("#board")
 const gameTileEls = document.querySelectorAll(".square")
 const btn = document.querySelector("button")
+const messageEl = document.querySelector("#messages")
 
 
 /*----- event listeners -----*/
@@ -22,26 +32,33 @@ btn.addEventListener('click', init)
 
 function init() {
     currentPlayer = 1
+    boardArray = [null, null, null, null, null, null, null, null, null]
+    winner = null
     render()
 
 }
 
 function render() {
-    gameTileEls.forEach(function (tile) {
-        tile.innerText = ""
-    })
+    
+    renderBoard()
+    renderMessage()
+
+   
+}
+
+function renderMessage()) {
+   if (winner) {
+    
+   } 
+   if (currentPlayer === 1)
 
 }
 
-function renderMarks(evt) {
-    if (currentPlayer === 1) {
-        evt.target.innerText = "X"
-        currentPlayer = 2
-    } else if (currentPlayer === 2) {
-        evt.target.innerText = "O"
-        currentPlayer = 1
-    }
+function renderBoard() {
+    /// loop over 9 elements of the board tiles, and for each one, check the board array at that index to determine if a mark should be placed, then place that color if so
+
 }
+
 
 function handleClick(evt) {
     if (evt.target.tagName !== "DIV") {
