b5e983e74c980800b1688699d7762b266b9048ca
mgballou <matthewbuiltthat@gmail.com>
2023-02-28 20:46:48 -0500

    finished win condition, added minimal styling

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 /*----- constants -----*/
 const colors = {
-    null: 'white',
+    null: 'lightblue',
     1: 'yellow',
-    '-1': 'pink',
+    '-1': 'orange',
 }
 
 const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]]
@@ -48,10 +48,10 @@ function render() {
 
 function renderMessage() {
     if (winner === null) {
-    messageEl.textContent = `${colors[currentPlayer].toUpperCase()}'s turn to play.`
+        messageEl.textContent = `${colors[currentPlayer].toUpperCase()}'s turn to play.`
     } else if (winner === 't') {
-    messageEl.textContent = "Tie game!"
-    } else if (winner){
+        messageEl.textContent = "Tie game!"
+    } else if (winner) {
         messageEl.textContent = `The winner is ${colors[winner].toUpperCase()}`
     }
 
@@ -63,44 +63,55 @@ function renderBoard() {
     /// loop over 9 elements of the board tiles, and for each one, check the board array at that index to determine if a mark should be placed, then place that color if so
     gameTileEls.forEach(function (tile) {
 
-        tile.style.backgroundColor = colors[boardArray[tile.id]]})
+        tile.style.backgroundColor = colors[boardArray[tile.id]]
+    })
 }
 
 
 function handleClick(evt) {
-        if (evt.target.tagName !== "DIV") {
-            return
-        }
-        console.log([boardArray[8]])
-        if (boardArray[evt.target.id]) {
-            return
-        }
-        if (winner) {
-            return
-        }
-        boardArray[evt.target.id] = currentPlayer
-        advanceTurn()
-        checkWin()
-        render()
-
+    if (evt.target.tagName !== "DIV") {
+        return
     }
+    if (boardArray[evt.target.id]) {
+        return
+    }
+    if (winner) {
+        return
+    }
+    boardArray[evt.target.id] = currentPlayer
+    advanceTurn()
+    checkWin()
+    render()
+
+}
 
 function advanceTurn() {
-        if (currentPlayer === 1) {
-            currentPlayer = -1
-        } else if (currentPlayer === -1) {
-            currentPlayer = 1
-        }
+    if (currentPlayer === 1) {
+        currentPlayer = -1
+    } else if (currentPlayer === -1) {
+        currentPlayer = 1
+    }
 }
 
 function checkWin() {
-    winConditions.forEach(function(combo) {
-        combo.forEach(function(index) {
-            let positionsTotal = 0
+    winConditions.forEach(function (combo) {
+        let positionsTotal = 0
+        combo.forEach(function (index) {
             positionsTotal = positionsTotal + boardArray[index]
-
-        }))
+        })
+        if (Math.abs(positionsTotal) === 3) {
+            winner = boardArray[combo[0]]
+        }
     })
+    let nullCount = 0
+    for (let i = 0; i < boardArray.length; i++) {
+        if (boardArray[i] === null) {
+            nullCount++
+        }
+    } if (nullCount === 0) {
+        winner = 't'
+    }
 }
+
 
 init()
